3e106ad2062e6625ee1f76f8c7853f12a353bdd5
Bruno Santos <34127252+BrunoSantos-Git@users.noreply.github.com>
2020-01-30 14:46:37 +0000

    Commit

---
+++ Visual Studio/Fanuc Hardware Generator/MainForm.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Fanuc_Hardware_Generator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lbFileName = new System.Windows.Forms.Label();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.lbSavePath = new System.Windows.Forms.Label();
+            this.txtSavePath = new System.Windows.Forms.TextBox();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridViewHardware = new System.Windows.Forms.DataGridView();
+            this.colName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colIOAddress = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.treeViewHardware = new System.Windows.Forms.TreeView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.lbInfo = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewHardware)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnGenerate.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.Location = new System.Drawing.Point(343, 487);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(400, 34);
+            this.btnGenerate.TabIndex = 4;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = false;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // lbFileName
+            // 
+            this.lbFileName.AutoSize = true;
+            this.lbFileName.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbFileName.Location = new System.Drawing.Point(4, 15);
+            this.lbFileName.Name = "lbFileName";
+            this.lbFileName.Size = new System.Drawing.Size(76, 19);
+            this.lbFileName.TabIndex = 5;
+            this.lbFileName.Text = "File name:";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFileName.Location = new System.Drawing.Point(86, 12);
+            this.txtFileName.MaxLength = 25;
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(150, 27);
+            this.txtFileName.TabIndex = 6;
+            this.txtFileName.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lbSavePath
+            // 
+            this.lbSavePath.AutoSize = true;
+            this.lbSavePath.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSavePath.Location = new System.Drawing.Point(4, 52);
+            this.lbSavePath.Name = "lbSavePath";
+            this.lbSavePath.Size = new System.Drawing.Size(76, 19);
+            this.lbSavePath.TabIndex = 7;
+            this.lbSavePath.Text = "Save path:";
+            // 
+            // txtSavePath
+            // 
+            this.txtSavePath.Enabled = false;
+            this.txtSavePath.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSavePath.Location = new System.Drawing.Point(8, 84);
+            this.txtSavePath.Name = "txtSavePath";
+            this.txtSavePath.Size = new System.Drawing.Size(380, 23);
+            this.txtSavePath.TabIndex = 8;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelect.Location = new System.Drawing.Point(86, 51);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 24);
+            this.btnSelect.TabIndex = 9;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtSavePath);
+            this.panel1.Controls.Add(this.btnSelect);
+            this.panel1.Controls.Add(this.txtFileName);
+            this.panel1.Controls.Add(this.lbFileName);
+            this.panel1.Controls.Add(this.lbSavePath);
+            this.panel1.Location = new System.Drawing.Point(343, 279);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 125);
+            this.panel1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(238, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 19);
+            this.label1.TabIndex = 10;
+            this.label1.Text = ".cfg";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Fanuc_Hardware_Generator.Properties.Resources.introsys_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(343, 418);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 54);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGridViewHardware
+            // 
+            this.dataGridViewHardware.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridViewHardware.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewHardware.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colName,
+            this.colIOAddress});
+            this.dataGridViewHardware.Location = new System.Drawing.Point(343, 12);
+            this.dataGridViewHardware.Name = "dataGridViewHardware";
+            this.dataGridViewHardware.RowHeadersWidth = 51;
+            this.dataGridViewHardware.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewHardware.Size = new System.Drawing.Size(400, 211);
+            this.dataGridViewHardware.TabIndex = 15;
+            this.dataGridViewHardware.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.dataGridViewHardware_EditingControlShowing);
+            this.dataGridViewHardware.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridViewHardware_KeyDown);
+            // 
+            // colName
+            // 
+            this.colName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colName.HeaderText = "Name";
+            this.colName.MaxInputLength = 50;
+            this.colName.MinimumWidth = 6;
+            this.colName.Name = "colName";
+            this.colName.ReadOnly = true;
+            // 
+            // colIOAddress
+            // 
+            this.colIOAddress.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.colIOAddress.DefaultCellStyle = dataGridViewCellStyle1;
+            this.colIOAddress.HeaderText = "IOAddress";
+            this.colIOAddress.MaxInputLength = 5;
+            this.colIOAddress.MinimumWidth = 6;
+            this.colIOAddress.Name = "colIOAddress";
+            this.colIOAddress.Width = 81;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(343, 229);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(99, 34);
+            this.btnAdd.TabIndex = 16;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.Location = new System.Drawing.Point(644, 229);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(99, 34);
+            this.btnRemove.TabIndex = 17;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // treeViewHardware
+            // 
+            this.treeViewHardware.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.treeViewHardware.Location = new System.Drawing.Point(12, 41);
+            this.treeViewHardware.Name = "treeViewHardware";
+            this.treeViewHardware.Size = new System.Drawing.Size(318, 481);
+            this.treeViewHardware.TabIndex = 18;
+            this.treeViewHardware.KeyDown += new System.Windows.Forms.KeyEventHandler(this.treeViewHardware_KeyDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 19);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Search:";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(70, 11);
+            this.txtSearch.MaxLength = 50;
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(260, 23);
+            this.txtSearch.TabIndex = 20;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // lbInfo
+            // 
+            this.lbInfo.AutoSize = true;
+            this.lbInfo.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbInfo.ForeColor = System.Drawing.Color.Black;
+            this.lbInfo.Location = new System.Drawing.Point(12, 532);
+            this.lbInfo.Name = "lbInfo";
+            this.lbInfo.Size = new System.Drawing.Size(144, 15);
+            this.lbInfo.TabIndex = 21;
+            this.lbInfo.Text = "S7 - Hardware Generator";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 556);
+            this.Controls.Add(this.lbInfo);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.treeViewHardware);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataGridViewHardware);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Fanuc Hardware Generator";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewHardware)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lbFileName;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label lbSavePath;
+        private System.Windows.Forms.TextBox txtSavePath;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridViewHardware;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.TreeView treeViewHardware;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label lbInfo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colIOAddress;
+    }
+}
+
---
+++ Visual Studio/Fanuc Hardware Generator/MainForm.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Fanuc_Hardware_Generator
+{
+    public partial class MainForm : Form
+    {
+        private string savePath;
+        private List<TreeNode> treeNodes;
+        private string libraryFolder;
+        private string libraryComponents;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            string workingDirectory = Environment.CurrentDirectory;
+            libraryFolder = Path.Combine(workingDirectory, "Library");
+            libraryComponents = Path.Combine(workingDirectory, "Library", "components");
+            treeNodes = new List<TreeNode>();
+
+            if (!Directory.Exists(libraryFolder) || !Directory.Exists(libraryComponents))
+            {
+                MessageBox.Show("Library not found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                DisableControls(this);
+            }
+            else
+            {
+                PopulateTree();
+            }
+        }
+
+        #region Events
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            TreeNode tn = new TreeNode("Hardware");
+
+            foreach (var node in treeNodes.Where(s => s.Name.ToLower().Contains(txtSearch.Text.ToLower())))
+            {
+                tn.Nodes.Add((TreeNode)node.Clone());
+            }
+
+            if (tn.Nodes.Count == 0)
+                tn.Text = "Nothing found";
+
+            treeViewHardware.Nodes.Clear();
+            treeViewHardware.Nodes.Add(tn);
+            treeViewHardware.ExpandAll();
+        }
+
+        private void treeViewHardware_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right && treeViewHardware.SelectedNode != null)
+            {
+                AddHardware();
+            }
+        }
+
+        private void dataGridViewHardware_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left && dataGridViewHardware.SelectedRows.Count > 0)
+            {
+                RemoveHardware();
+            }
+        }
+
+        private void dataGridViewHardware_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            e.Control.KeyPress -= new KeyPressEventHandler(IOColumn_KeyPress);
+            if (dataGridViewHardware.CurrentCell.ColumnIndex == 1)
+            {
+                if (e.Control is TextBox tb)
+                {
+                    tb.KeyPress += new KeyPressEventHandler(IOColumn_KeyPress);
+                }
+            }
+        }
+
+        private void IOColumn_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+        
+        #region Button Events
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            var pathSave = new FolderBrowserDialog();
+
+            if (pathSave.ShowDialog() == DialogResult.OK)
+            {
+                savePath = pathSave.SelectedPath;
+                txtSavePath.Text = savePath;
+            }
+
+            pathSave.Dispose();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            AddHardware();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            RemoveHardware();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            string fileName = txtFileName.Text + ".cfg";
+            string savePath = txtSavePath.Text;
+            var rows = dataGridViewHardware.Rows;
+
+            if (CanGenerate(fileName, savePath, rows))
+            {
+                string filePath = Path.Combine(savePath, fileName);
+                string pcBasePath = Path.Combine(libraryFolder, "PC_Based.cfg");
+                if(!File.Exists(pcBasePath))
+                {
+                    MessageBox.Show("File \"PC_Based.cfg\" not found in Library folder", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                string[] pcBase = File.ReadAllLines(pcBasePath);
+                pcBase = pcBase.Select(x => x.Replace("%currdate", DateTime.Now.ToString())).ToArray();
+
+                List<string[]> componentsCfgFiles = new List<string[]>();
+                foreach (DataGridViewRow row in dataGridViewHardware.Rows)
+                {
+                    if (row.Cells[0].Value != null)
+                    {
+                        string nameCol = row.Cells[0].Value.ToString();
+                        string ioCol = row.Cells[1].Value.ToString();
+                        string tag = row.Tag.ToString();
+                        string formatedName = nameCol.ToLower().Replace(" ", "-");
+
+                        if (!string.IsNullOrEmpty(GetFileByName(formatedName)))
+                        {
+                            string[] lines = ReplaceIOAddress(formatedName, tag, ioCol);
+                            componentsCfgFiles.Add(lines);
+                        }
+                    }
+                }
+
+                File.WriteAllLines(filePath, pcBase);
+                File.AppendAllText(filePath, "\n");
+                foreach (var item in componentsCfgFiles)
+                {
+                    File.AppendAllLines(filePath, item);
+                    File.AppendAllText(filePath, "\n");
+                }
+
+                MessageBox.Show("File Successfully Generated to Path: \n\"" + filePath + "\"", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        #endregion
+
+        #endregion
+
+        #region Private
+        private void PopulateTree()
+        {
+            TreeNode tn = new TreeNode("Hardware");
+
+            foreach (var file in Directory.GetFiles(libraryComponents))
+            {
+                if(Path.GetExtension(file).Contains("cfg"))
+                {
+                    string io = GetIOAddress(file);
+                    string fileName = Path.GetFileNameWithoutExtension(file).ToUpper();
+                    fileName = fileName.Replace("-", " ");
+                    TreeNode comp = new TreeNode
+                    {
+                        Name = Path.GetFileNameWithoutExtension(file),
+                        Tag = io,
+                        Text = fileName
+                    };
+                    treeNodes.Add(comp);
+                    tn.Nodes.Add(comp);
+                }
+            }
+
+            treeViewHardware.Nodes.Add(tn);
+            treeViewHardware.ExpandAll();
+        }
+        
+        private string GetFileByName(string s)
+        {
+            return Directory.GetFiles(libraryComponents).FirstOrDefault(f => Path.GetFileNameWithoutExtension(f).Contains(s));
+        }
+
+        private void DisableControls(Control con)
+        {
+            foreach (Control c in con.Controls)
+            {
+                DisableControls(c);
+            }
+            con.Enabled = false;
+        }
+        
+        private bool CanGenerate(string fileName, string savePath, DataGridViewRowCollection rows)
+        {
+            if (string.IsNullOrEmpty(fileName) || string.IsNullOrEmpty(savePath))
+                return false;
+
+            if (!IsValidFilename(fileName))
+            {
+                MessageBox.Show("Invalid file name", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            List<string> IOs = new List<string>();
+
+            if (rows.Count > 0)
+            {
+                for (int i = 0; i < rows.Count-1; i++)
+                {
+                    if (rows[i].Cells[0].Value == null)
+                        return false;
+
+                    if (rows[i].Cells[1].Value != null)
+                        IOs.Add(rows[i].Cells[1].Value.ToString());
+                }
+            }
+            else
+                return false;
+
+            if (IOs.Count != IOs.Distinct().Count())
+            {
+                MessageBox.Show("IO Addresses must be different", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
+        
+        private void AddHardware()
+        {
+            var selected = treeViewHardware.SelectedNode;
+            if (selected != null && !selected.Text.Equals("Hardware"))
+            {
+                int index = dataGridViewHardware.Rows.Add(selected.Text, selected.Tag);
+                dataGridViewHardware.Rows[index].Tag = selected.Tag;
+            }
+        }
+        
+        private void RemoveHardware()
+        {
+            var selectedRows = dataGridViewHardware.SelectedRows;
+            if (selectedRows.Count > 0)
+            {
+                var selectedRow = selectedRows[0];
+                if ((selectedRow.Index + 1) != dataGridViewHardware.Rows.Count) // is not last row
+                {
+                    dataGridViewHardware.Rows.Remove(selectedRow);
+                }
+            }
+        }
+        
+        private string GetIOAddress(string path)
+        {
+            string io = "";
+
+            string[] lines = File.ReadAllLines(path);
+            foreach (var line in lines)
+            {
+                if(line.ToLower().Contains("ioaddress"))
+                {
+                    string[] substring = line.Split(',');
+                    foreach (var s in substring)
+                    {
+                        if(s.ToLower().Contains("ioaddress"))
+                        {
+                            io = s.ToLower().Replace("ioaddress", string.Empty).Replace(" ", string.Empty);
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+
+            return io;
+        }
+        
+        private string[] ReplaceIOAddress(string fileName, string oldIO, string newIO)
+        {
+            string[] lines = File.ReadAllLines(Path.Combine(libraryComponents, fileName + ".cfg"));
+            lines = lines.Select(x => x.Replace("IOADDRESS " + oldIO, "IOADDRESS " + newIO)).ToArray();
+            return lines;
+        }
+
+        private bool IsValidFilename(string filename)
+        {
+            foreach (var ch in Path.GetInvalidFileNameChars())
+            {
+                if (filename.Contains(ch))
+                    return false;
+            }
+
+            return true;
+        }
+        #endregion
+    }
+}
+
